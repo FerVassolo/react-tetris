870cc0e4e3b4ccfe26ec09b74ba41d998afa6401
FerVassolo <63514959+Parrandos@users.noreply.github.com>
2025-07-18 11:04:35 -0300

    Feat: trying to deploy version 0.1.0

--- src/hooks/useKeyboardControls.ts
+++ src/hooks/useKeyboardControls.ts
@@ -1,27 +1,50 @@
 import React from 'react';
-import key from 'keymaster';
 import DetectShift from '../modules/detect-shift';
 import { Action } from '../models/Game';
 
 export type KeyboardMap = Record<string, Action>;
+
+type KeyboardDispatch = Record<string, () => void>;
+
+type Keymaster = {
+  (key: string, fn: () => void): void;
+  unbind: (key: string) => void;
+};
 
 export const useKeyboardControls = (
   keyboardMap: KeyboardMap,
   dispatch: React.Dispatch<Action>
 ): void => {
   React.useEffect(() => {
-    const keyboardDispatch = Object.entries(
-      keyboardMap
-    ).reduce<KeyboardDispatch>((output, [key, action]) => {
-      output[key] = () => dispatch(action);
-      return output;
-    }, {});
-    addKeyboardEvents(keyboardDispatch);
-    return () => removeKeyboardEvents(keyboardDispatch);
+    let key: Keymaster | undefined;
+    let removeKeyboardEvents: ((keyboardMap: KeyboardDispatch) => void) | undefined;
+
+    import('keymaster').then((keymasterModule) => {
+      key = (keymasterModule.default || keymasterModule) as Keymaster;
+      const keyboardDispatch = Object.entries(
+        keyboardMap
+      ).reduce<KeyboardDispatch>((output, [key, action]) => {
+        output[key] = () => dispatch(action);
+        return output;
+      }, {});
+      addKeyboardEvents(keyboardDispatch, key);
+      removeKeyboardEvents = (keyboardMap) => {
+        Object.keys(keyboardMap).forEach((k) => {
+          if (k === 'shift') {
+            const fn = keyboardMap[k];
+            fn && DetectShift.unbind(fn);
+          } else {
+            key && key.unbind(k);
+          }
+        });
+      };
+      // Cleanup
+      return () => removeKeyboardEvents && removeKeyboardEvents(keyboardDispatch);
+    });
   }, [keyboardMap, dispatch]);
 };
 
-function addKeyboardEvents(keyboardMap: KeyboardDispatch) {
+function addKeyboardEvents(keyboardMap: KeyboardDispatch, key: Keymaster) {
   Object.keys(keyboardMap).forEach((k: keyof KeyboardDispatch) => {
     const fn = keyboardMap[k];
     if (k === 'shift' && fn) {
@@ -31,15 +54,3 @@ function addKeyboardEvents(keyboardMap: KeyboardDispatch) {
     }
   });
 }
-function removeKeyboardEvents(keyboardMap: KeyboardDispatch) {
-  Object.keys(keyboardMap).forEach((k) => {
-    if (k === 'shift') {
-      const fn = keyboardMap[k];
-      fn && DetectShift.unbind(fn);
-    } else {
-      key.unbind(k);
-    }
-  });
-}
-
-type KeyboardDispatch = Record<string, () => void>;
